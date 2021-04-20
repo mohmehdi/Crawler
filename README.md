dfd5efa57b1be66ece75f45c3b03cde068f884ac
moh <m.m.afkhami@outlook.com>
2021-04-20 20:22:32 +0430

    readme added

---
+++ README.md
@@ -0,0 +1,18 @@
+# Job Crawler
+
+### how to use it:
+ `scrapy crawl jobsp -a urls="www.freelancer.com/projects/1 www.freelancer.com/projects/2" -a depth=2 --nolog -O out.json`
+
++ **`-a`** is for adding arguments to spider  
++ **`--nolog`** means no log obviously  
++ **`-o out.json`**  saves file in json format  
+**`-O`** does the same and overwrites the json file
+##### Adding URLs:
+from [freelancer website](freelancer.com) take a job url   
+use `-a urls=` and put it in these `" "` make sure to leave a space between them  like this --> **"** url1 url2 url3 **"**  
+#####  Set Depth to Crawl
+**`-a depth`** means how deep to crawl and if set to 0 it just crawls starting urls  , by defualt its 1
+
+##### Using Crawled Data
+for using crawled jobs run **main.py** (recommended to run it in a console)  
+it just shows data in a more human friendly way
--- job_crawler/job_crawler/spiders/job_spider.py
+++ job_crawler/job_crawler/spiders/job_spider.py
@@ -1,7 +1,4 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
-
-
 class JobSpider(scrapy.Spider):
     name = "jobsp"
     allowed_domains = ['www.freelancer.com']
@@ -9,7 +6,9 @@ class JobSpider(scrapy.Spider):
     urls = ""
     count = 0
     id = 0
-
+    #custom_settings = {
+    #    'DOWNLOAD_DELAY': 0.5,
+    #}
 
     def start_requests(self):
         urls = self.urls.split(' ')
@@ -22,6 +21,7 @@ class JobSpider(scrapy.Spider):
 
         c_depth = response.meta.get('depth')
 
+        print(f"{self.id} --> depth: {c_depth}")
 
         detail = ''.join(
             response.xpath('//*[@id="main"]/div/div/div/div[1]/section[1]/div/div[1]/div/p[not(@*)]/text()').getall())
@@ -42,32 +42,38 @@ class JobSpider(scrapy.Spider):
             similar_jobs = response.xpath(
                 '//*[@id="main"]/div/div/div/div[2]/aside/section[2]/div/div[2]/ul/li/a/@href').getall()
             self.count+=len(similar_jobs)
-            print(f"{self.id} --> depth: {c_depth} found {len(similar_jobs)} links . total: {self.count}")
+            print(f" found {len(similar_jobs)} links . total links: {self.count}")
 
             for href in similar_jobs:
+                #print(href)
                 yield response.follow(href, callback=self.parse, meta={'depth': (c_depth + 1)})
 
-    # job title, location, skills, details, time expire, budget, similar jobs
-    # print(response.xpath('//h1[@class="PageProjectViewLogout-header-title"]/text()').get())
 
-    # job title
-    # response.xpath('//h1[@class="PageProjectViewLogout-header-title"]/text()').get()
+""" xpaths
 
-    # location
-    # response.xpath('//span[@class="PageProjectViewLogout-detail-reputation-item-locationItem"]/text()').get().strip()
+     job title, location, skills, details, time expire, budget, similar jobs
+     print(response.xpath('//h1[@class="PageProjectViewLogout-header-title"]/text()').get())
 
-    # skills
-    # response.xpath('//a[@class="PageProjectViewLogout-detail-tags-link--highlight"]/text()').getall()
+     job title
+     response.xpath('//h1[@class="PageProjectViewLogout-header-title"]/text()').get()
 
-    # details
-    # response.xpath('//*[@id="main"]/div/div/div/div[1]/section[1]/div/div[1]/div/p[not(@*)]/text()').getall()
-    # detail = (response.xpath('//*[@id="main"]/div/div/div/div[1]/section[1]/div/div[1]/div/p[not(@*)]/text()').getall())
+     location
+     response.xpath('//span[@class="PageProjectViewLogout-detail-reputation-item-locationItem"]/text()').get().strip()
 
-    # time expire
-    # response.xpath('//span[@class="promotion-tag PageProjectViewLogout-promotionTag"]/text()').get().strip()
+     skills
+     response.xpath('//a[@class="PageProjectViewLogout-detail-tags-link--highlight"]/text()').getall()
 
-    # budget
-    # response.xpath('//p[@class="PageProjectViewLogout-header-byLine"]/text()').get()
+     details
+     response.xpath('//*[@id="main"]/div/div/div/div[1]/section[1]/div/div[1]/div/p[not(@*)]/text()').getall()
+     detail = (response.xpath('//*[@id="main"]/div/div/div/div[1]/section[1]/div/div[1]/div/p[not(@*)]/text()').getall())
 
-    # similar jobs
-    # //response.xpath('//*[@id="main"]/div/div/div/div[2]/aside/section[2]/div/div[2]/ul/li/a/@href').getall()
+     time expire
+     response.xpath('//span[@class="promotion-tag PageProjectViewLogout-promotionTag"]/text()').get().strip()
+
+     budget
+     response.xpath('//p[@class="PageProjectViewLogout-header-byLine"]/text()').get()
+
+     similar jobs
+     response.xpath('//*[@id="main"]/div/div/div/div[2]/aside/section[2]/div/div[2]/ul/li/a/@href').getall()
+    """
+
